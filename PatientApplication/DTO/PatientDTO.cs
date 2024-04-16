73ac1116d8309afea92dc3f69497c845627f4c2e
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-04-16 13:48:40 +0200

    Changed the SLN file, added some crud to the Patient.

---
+++ PatientApplication/DTO/PatientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientApplication.DTO
+{
+    public class PatientDTO
+    {
+        public string SSN { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
--- PatientInfrastructure/RepositoryDBContext.cs
+++ PatientInfrastructure/RepositoryDBContext.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Patientinfrastructure
+namespace PatientInfrastructure
 {
     public class RepositoryDBContext : DbContext
     {

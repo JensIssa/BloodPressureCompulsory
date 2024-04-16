d0db1d01f73167c3d186082e933fad04e69cb98e
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-04-16 12:55:30 +0200

    Removed object reference that hasn't been created yet.

--- Domain/Patient.cs
+++ Domain/Patient.cs
@@ -24,7 +24,7 @@ namespace Domain
         /// <summary>
         /// 
         /// </summary>
-        public ICollection<Measurement> measurements { get; set; }
+      //  public ICollection<Measurement> measurements { get; set; }
 
     }
 }

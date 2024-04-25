2f2d885a3cd5265f2c2b0f41727c4b36fe6457d9
Matty <Mathiaskrarupmadsen@gmail.com>
2024-04-25 18:54:14 +0200

    added packages and modified program.cs

--- MeasurementService/Program.cs
+++ MeasurementService/Program.cs
@@ -6,6 +6,9 @@ using MeasurementApplication.Interfaces;
 using MeasurementInfrastructure;
 using MeasurementInfrastructure.Interfaces;
 using MeasurementRepository;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region 
+builder.Services.AddOpenTelemetry().WithTracing(builder => builder
+.AddAspNetCoreInstrumentation()
+.SetSampler(new AlwaysOnSampler())
+.AddSource("MeasurementService")
+.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MeasurementService"))
+.AddZipkinExporter(options =>
+{
+    options.Endpoint = new Uri("http://zipkin:9411/api/v2/spans");
+}));
+
+#endregion
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Seq("http://seq:5341")
+    .WriteTo.Console()
+    .CreateLogger();
 
 #region AutoMapper
 var mapper = new MapperConfiguration(config =>

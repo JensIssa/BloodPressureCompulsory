8e3b5e527db4590beb9593546a2a1299dd67313b
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-04-29 03:55:11 +0200

    Added the location fix, so only users can from Denmark can access the service.

--- Frontend/angular-PatientUI/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/angular-PatientUI/src/app/components/dashboard/dashboard.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MeasurementService} from "../../services/Measurement.service";
 import {FormsModule} from "@angular/forms";
 import {CreateMeasurementModel} from "../../models/create-measurement.model";
+import {GeoLocationService} from "../../services/geolocation.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   measurement: CreateMeasurementModel = new CreateMeasurementModel(0,0, '');
+  isVisible = false;
 
-  constructor(private measurementService: MeasurementService) {}
+
+  constructor(private measurementService: MeasurementService, private geoLocationService: GeoLocationService) {}
+
+  ngOnInit(): void {
+    this.geoLocationService.getUserCountry().subscribe(
+      country => {
+        if (country === 'Denmark') {
+          this.isVisible = true;
+        }
+      },
+      error => {
+        console.error('Error retrieving user country:', error);
+      }
+    );
+  }
 
   addMeasurement(): void {
     if (this.measurement.systolic > 0 && this.measurement.diastolic > 0 && this.measurement.patientSSN) {
